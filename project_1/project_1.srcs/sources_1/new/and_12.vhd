f54401e0bdf45cc0db35e81de15f78e50680ab78
Naren Subburaj <narensraj21@gmail.com>
2025-11-24 07:08:21 -0600

    auto-commit

---
+++ project_1/project_1.srcs/sources_1/new/and_12.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/24/2025 07:06:41 AM
+-- Design Name: 
+-- Module Name: and_12 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity and_12 is
+    Port ( a,b : in STD_LOGIC;
+           y : out STD_LOGIC);
+end and_12;
+
+architecture Behavioral of and_12 is
+
+begin
+
+
+end Behavioral;
