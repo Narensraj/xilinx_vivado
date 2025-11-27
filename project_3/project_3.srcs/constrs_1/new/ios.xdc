1d63717111fc56c0ce230b77519c1cc6206e03fc
Naren Subburaj <narensraj21@gmail.com>
2025-11-27 06:53:50 -0600

    I/O Planning

---
+++ project_3/project_3.srcs/constrs_1/new/ios.xdc
@@ -0,0 +1,12 @@
+set_property -dict {PACKAGE_PIN G15 IOSTANDARD LVCMOS33} [get_ports {sw[0]}]
+set_property -dict {PACKAGE_PIN P15 IOSTANDARD LVCMOS33} [get_ports {sw[1]}]
+set_property -dict {PACKAGE_PIN W13 IOSTANDARD LVCMOS33} [get_ports {sw[2]}]
+set_property -dict {PACKAGE_PIN T16 IOSTANDARD LVCMOS33} [get_ports {sw[3]}]
+set_property -dict {PACKAGE_PIN M14 IOSTANDARD LVCMOS33} [get_ports {led[0]}]
+set_property -dict {PACKAGE_PIN M15 IOSTANDARD LVCMOS33} [get_ports {led[1]}]
+set_property -dict {PACKAGE_PIN G14 IOSTANDARD LVCMOS33} [get_ports {led[2]}]
+set_property -dict {PACKAGE_PIN D18 IOSTANDARD LVCMOS33} [get_ports {led[3]}]
+set_property IOSTANDARD LVCMOS18 [get_ports clk]
+set_property PACKAGE_PIN K17 [get_ports clk]
+#revert back to original instance
+current_instance -quiet
---
+++ project_3/project_3.srcs/sources_1/imports/project_3/io_1.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.1 (win64) Build 3526262 Mon Apr 18 15:48:16 MDT 2022
+// Date        : Thu Nov 27 06:52:30 2025
+// Host        : DESKTOP-GU4IL4Q running 64-bit major release  (build 9200)
+// Command     : write_verilog -mode pin_planning -force -port_diff_buffers E:/xilinx_vivado/project_3/io_1.v
+// Design      : ios
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+module ios(sw, led, clk);
+  inout [3:0] sw;
+  inout [3:0] led;
+  input clk;
+
+  // internal wires associated with differential buffers
+
+
+  // differential buffers
+
+
+endmodule
